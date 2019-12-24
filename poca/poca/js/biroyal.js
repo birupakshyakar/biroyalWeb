e93f0a773e2fc4c086c299cf88cd492289659ad0
dashprd <dash.prd@gmail.com>
2019-12-24 08:12:32 +0530

    new changes

---
+++ poca/poca/js/biroyal.js
@@ -0,0 +1,5 @@
+/*$(".classynav ul li a").on('click', function(){
+	//alert("test");
+	$(".classynav ul li.current-item").removeClass('current-item');
+	$(this).parents('li').addClass('current-item');
+});*/
